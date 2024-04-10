90e0a500d71f7812bf24acf3238eb084e496efeb
Dtibamoza96 <152095273+Dtibamoza96@users.noreply.github.com>
2024-04-10 21:01:03 +0200

    Rama Slider actualizada

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,14 @@
-import "./App.css";
-import { opciones } from "./data/opciones";
-import { opcionesI } from "./data/opcionesI";
-import Opciones from "./Components/Opciones";
-import OpcionesI from "./Components/OpcionesI";  
-import OpcionesView from "./views/OpcionesView";
-import OpcionesIviews from "./views/OpcionesIviews";
-import {Routes, Route } from "react-router-dom";
-
+import './App.css'
+import Cartelera from './Components/Cartelera'
 
 function App() {
+
   return (
     <>
-    <Routes> 
-      <Route path="*"  element= {<Home />} />
-      {opciones.map((opcion, index) => (
-        <Route key={index} path={"/" + opcion.name.toLowerCase()} element={<OpcionesView {...opcion} />} />
-      ))}
-      {opcionesI.map((opcionI, indexI) => (
-        <Route key={indexI} path={"/" + opcionI.nameI.toLowerCase()} element={<OpcionesIviews {...opcionI} />} />
-      ))}
-    </Routes>
+      <Cartelera />
     </>
-  );
+      
+  )
 }
 
-
-function Home() {
-  return (
-    <div>
-      <div className="footerContainer">
-        <div className="opcionesImagenes_footer">
-          {opcionesI.map((opcionI, index) => (
-            <OpcionesI key={index} {...opcionI} />
-          ))}
-        </div>
-       
-       <div className="opcionesTexto_footer">
-        {opciones.map((opcion, index) => (
-          <Opciones key={index} {...opcion} />
-        ))}
-       </div>
-       
-      </div>
-    </div>
-  );
-}
-
-
-
-export default App;
+export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
 
-
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter> 
-    <Routes>
-      <Route path="/*" element= {<App />} />
-    </Routes>
-    </BrowserRouter>
-  </React.StrictMode>
-);
+    <App />
+  </React.StrictMode>,
+)
